56a72165cae151b483163122391c609c823610d6
sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2023-04-28 15:14:30 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.35 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        int[] copy = Arrays.copyOf(emergency, emergency.length);
+        
+        Arrays.sort(copy);
+        
+        for (int i = 0; i < emergency.length; i++) {
+            for (int j = 0; j < copy.length; j++) {
+                if (emergency[i] == copy[j]) {
+                    answer[i] = emergency.length -j;
+                }
+            }
+        }
+        return answer;
+    }
+}
